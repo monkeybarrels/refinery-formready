a63eb02caae83e4a740b302bac00cc4585e66b0f
Jim Livingston <trkbrkr2000@gmail.com>
2025-10-25 21:36:19 -0400

    NUCLEAR OPTION: Force Railway deployment
    - Modify nuxt.config.ts with rebuild comment
    - Change build script to echo rebuild message
    - This should absolutely force Railway to rebuild
    - Multiple critical files changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// FORCE RAILWAY REBUILD - 2025-10-26
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
